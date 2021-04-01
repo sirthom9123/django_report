ad03fe0fec654e9c43410a25539a4c1c5adf139a
Tshiamo Motshabi <qraircorp@gmail.com>
2021-04-01 12:43:22 +0200

    Authentication

--- dj_ds/settings.py
+++ dj_ds/settings.py
@@ -112,7 +112,7 @@ USE_L10N = True
 USE_TZ = True
 
 #AUTHENTICATION
-LOGIN_URL = '/login/'
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
