957c3e96716f90dc81ad424e838cf1cb00a6285f
Tshiamo Motshabi <qraircorp@gmail.com>
2021-04-01 11:25:49 +0200

    Modifying Dropzone.js

--- sales/models.py
+++ sales/models.py
@@ -55,8 +55,8 @@ class Sale(models.Model):
 
 
 class CSV(models.Model):
-    file_name = models.FileField(upload_to='csv')
-    activated = models.BooleanField(default=False)
+    file_name = models.CharField(max_length=120, null=True)
+    csv_file = models.FileField(upload_to='csv', null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
--- static/js/upload.js
+++ static/js/upload.js
@@ -1,4 +1,12 @@
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+const alertBox = document.getElementById('alert-box')
+
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+    <div class="alert alert-${type}" role="alert">
+        ${msg}
+    </div>`
+}
 
 
 Dropzone.autoDiscover = false
@@ -8,6 +16,14 @@ const myDropzone = new Dropzone('#my-dropzone', {
         this.on('sending', function(file, xhr, formData){
             console.log('sending')
             formData.append('csrfmiddlewaretoken', csrf)
+        })
+        this.on('success', function(file, response){
+            console.log(response.ex)
+            if (response.ex){
+                handleAlerts('danger', 'Ooops file already exists')
+            }else{
+                handleAlerts('success', 'File uploaded successfully')
+            }
         })
     },
     maxFiles: 3,
